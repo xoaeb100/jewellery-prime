72d864707c92f4acf863099048a8564b90665b7b
ASHMIT079 <114645231+ASHMIT079@users.noreply.github.com>
2023-03-13 17:43:28 +0530

    edit-profile

--- src/app/profile/edit-profile/edit-profile.component.ts
+++ src/app/profile/edit-profile/edit-profile.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
-  styleUrls: ['./edit-profile.component.scss']
+  styleUrls: ['./edit-profile.component.scss'],
 })
 export class EditProfileComponent {
+  firstName!: string;
+  sureName!: string;
+  male!: string;
+  female!: string;
+  email!: string;
 
+  value8: any;
+
+  gender: { name: string; code: string }[];
+  activeIndex: number = 1;
+  constructor() {
+    this.gender = [
+      { name: 'Male', code: 'Male' },
+      { name: 'Female', code: 'Female' },
+    ];
+  }
 }
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { RouterModule, Routes } from '@angular/router';
 import { UiModule } from '../ui/ui.module';
+import { RadioButtonModule } from 'primeng/radiobutton';
 
 const routes: Routes = [{ path: '', component: EditProfileComponent }];
 
 @NgModule({
   declarations: [EditProfileComponent],
-  imports: [CommonModule, UiModule, [RouterModule.forChild(routes)]],
+  imports: [
+    CommonModule,
+    UiModule,
+    RadioButtonModule,
+    [RouterModule.forChild(routes)],
+  ],
 })
 export class ProfileModule {}
