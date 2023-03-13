99cb006deb9872b7884a610f17b2338d06acb3e8
xoaeb100 <shoaib100aib@gmail.com>
2023-03-13 10:06:04 +0530

    user model

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -11,40 +11,19 @@ export interface User {
   shopUniqueId?: string;
   aadhaarNo?: string;
   panNo?: string;
-
-  regId: string;
-  instUniqueNo?: number;
-  rollNo: string;
-  dob: Date;
-  div?: string;
-  std?: string;
-  board?: string;
-  checked?: boolean;
+  
 
   line1?: string;
   line2?: string;
   landmark?: string;
-  zip?: string;
+  pin?: string;
   country?: string;
   state?: string;
   city?: string;
   locality?: string;
   regionCode?: REGION_CODE;
 
-  fatherFirstName?: string;
-  fatherLastName?: string;
-  fatherEmail?: string;
-  fatherMobile?: string;
-  motherFirstName?: string;
-  motherLastName?: string;
-  motherEmail?: string;
-  motherMobile?: string;
-  guardianFirstName?: string;
-  guardianLastName?: string;
-  guardianEmail?: string;
-  guardianMobile?: string;
-  totalAmount?: number;
-  balance?: number;
+
 }
 
 export enum USER_TYPE {
